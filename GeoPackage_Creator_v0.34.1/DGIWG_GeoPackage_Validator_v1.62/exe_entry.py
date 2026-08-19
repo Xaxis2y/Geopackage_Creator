1e1a53d3dd00d33409fe5d612db4be95c0956107
Xaxis2y <143286427+Xaxis2y@users.noreply.github.com>
2026-08-19 10:47:07 -0400

    Delete GeoPackage_Creator_v0.34.1 directory

--- GeoPackage_Creator_v0.34.1/DGIWG_GeoPackage_Validator_v1.62/exe_entry.py
+++
@@ -1,9 +0,0 @@
-"""PyInstaller entry point for the v1.62 standalone executable."""
-# SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (c) 2026 Eui Soo SON
-
-from dgiwg_validator.main import main
-
-
-if __name__ == "__main__":
-    main()
